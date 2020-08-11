3b931fc6cd462f1de1a394c662d6f3d08408248e
LAPTOP-P6BOV00B\Rodolfo <rodolfo.ufrpe@gmail.com>
2020-08-11 16:22:43 -0300

    Adiciona Pipes e Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from '../notfound/notfound.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+       path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '**', component: NotFoundComponent
+      }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,36 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
   })
 export class CourseListComponent implements OnInit{
+    filteredCourses: Course[] = [];
     courses: Course[] = [];
-
+    _filterBy: string;
+    constructor(private courseService: CourseService){}
     ngOnInit(): void {
-        this.courses = [
-                {
-                id: 1,
-                name: 'Angular:Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-22',
-                duration: 120,
-                rating: 5.4,
-                releaseDate: 'December, 2, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular:HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LK-BLABLA',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'December, 2, 2019'
-            }
-        ];
+        this.courses = this.courseService.retrieveAll();
+        this.filteredCourses = this.courses;
     }
-    
+    set filter(value: string){
+        this._filterBy = value;
+        this.filteredCourses = this.courses.filter((course: Course) => 
+        course.name.toLocaleLowerCase().indexOf(this._filterBy.toLowerCase()) > -1);
+    }
+    get filter(): string {
+        return this._filterBy;
+    }
+
 }
